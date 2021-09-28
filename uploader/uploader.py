0360a249edb69273f7e0c7201e25bf8b185c06f5
Thomas <57842400+OpenScanEu@users.noreply.github.com>
2021-09-28 10:31:17 +0200

    Update uploader.py

--- uploader/uploader.py
+++ uploader/uploader.py
@@ -5,9 +5,9 @@ from zipfile import ZipFile
 
 ################ That's all you need to change : ###################
 
-dir_images = '/images/' # enter the directory of your images
-dir_temp = '/temp/' # enter your temporary directory
-token = ''# enter your token (send me a mail to cloud@openscan.eu with your forename and surname to get a free token)
+dir_images = '' # enter the directory of your images
+dir_temp = '' # enter your temporary directory
+token = '' # enter your token (send me a mail to cloud@openscan.eu with your forename and surname to get a free token)
 
 ################ No need to change anything below ##################
 
@@ -22,6 +22,11 @@ server = 'http://openscanfeedback.dnsuser.de:1334/'
 msg= {}
 msg['token'] = token
 
+def stop(msg):
+    print(msg)
+    while True:
+        pass
+
 def OpenScanCloud(cmd, msg):
     r = requests.get(server + cmd, auth=(user, pw), params=msg)
     if r.status_code == 200:
@@ -34,16 +39,24 @@ def uploadAndStart(filelist, ulinks):
     for file in filelist:
         print('uploading part ' + str(i+1) + ' of ' + str(len(filelist)))
         link = ulinks[i]
-        cmd = 'curl -# -X POST ' + link + ' --header Content-Type:application/octet-stream --data-binary @"' + file
         i = i+1
-        os.system(cmd)
+
+        url = link
+        files = {'file': open(file, 'rb')}
+        headers = {'Content-type': 'application/octet-stream'}
+
+        r = requests.post(url, headers=headers, files=files)
+        if r.status_code != 200:
+            stop('ERROR: could not upload file')
+        files.clear()
+
         os.remove(file)
     print('starting project')
     r = OpenScanCloud('startProject', msg)
     if r.status_code != 200:
-        print('ERROR: could not start processing')
+        stop('ERROR: could not start processing')
     print('processing started ... you will get an email soon')
-    print('thank you for testing OpenScanCloud')
+    stop('thank you for testing OpenScanCloud')
 
 def getAndVerifyToken():
     print('verifying token')
@@ -53,8 +66,7 @@ def getAndVerifyToken():
 
     tokenInfo = OpenScanCloud('getTokenInfo', msg)
     if tokenInfo.status_code != 200:
-        print('ERROR: invalid token')
-        exit()
+        stop('ERROR: invalid token')
 
     limit_filesize = tokenInfo.json()['limit_filesize']
     limit_photos = tokenInfo.json()['limit_photos']
@@ -63,14 +75,13 @@ def getAndVerifyToken():
 def prepareSet():
     print('preparing imageset')
     # load images:
-    list = os.listdir(dir_images)
+    list = []
 
-    for i in list:
-        if os.path.splitext(i)[1] not in allowed_extensions:
-            list.remove(i)
+    for i in os.listdir(dir_images):
+        if os.path.splitext(i)[1] in allowed_extensions:
+            list.append(i)
     if len(list) == 0:
-        print('ERROR: no images found in ' + dir_images)
-        exit()
+        stop('ERROR: no images found in ' + dir_images)
 
     filesize = 0
     for i in list:
@@ -79,8 +90,7 @@ def prepareSet():
     msg['photos'] = len(list)
 
     if filesize > limit_filesize or len(list) > limit_photos:
-        print('ERROR: Limits exceeded')
-        exit()
+        stop('ERROR: Limits exceeded')
     return list
 
 def zipAndSplit(imagelist):
@@ -117,8 +127,7 @@ def zipAndSplit(imagelist):
     print('preparing project on the OpenScanCloud server')
     r = OpenScanCloud('createProject', msg)
     if r.status_code != 200:
-        print('ERROR: Could not create project')
-        exit()
+        stop('ERROR: Could not create project')
     msg['ulink'] = r.json()['ulink']
 
 getAndVerifyToken()
